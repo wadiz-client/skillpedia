f921ab3e2db6ac7268aa627a77e3f157c3225227
백은주 <specialguest@kakao.com>
2026-07-05 21:56:37 +0900

    feat: PrivateRepoSection 컴포넌트에 다국어 적용

--- src/views/(home)/_ui/PrivateRepoSection/PrivateRepoSection.tsx
+++ src/views/(home)/_ui/PrivateRepoSection/PrivateRepoSection.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { Box, Section, SectionIntro } from '@primer/react-brand';
+import { useTranslations } from 'next-intl';
 
 export const PrivateRepoSection = () => {
+  const t = useTranslations('HomePage.PrivateRepoSection');
+
   return (
     <Section as="section" backgroundColor="subtle" data-color-mode="dark">
       <Box
@@ -14,14 +17,14 @@ export const PrivateRepoSection = () => {
         <SectionIntro>
           <SectionIntro.Label>Private</SectionIntro.Label>
           <SectionIntro.Heading size="2" weight="extrabold">
-            사내 저장소 스킬을
-            <br />팀 안에서 안전하게
+            {t.rich('title', {
+              br: () => {
+                return <br />;
+              },
+            })}
           </SectionIntro.Heading>
-          <SectionIntro.Description>
-            사내 인프라에 직접 설치해서 운영해 보세요. 외부에 공개하지 않고 사내 저장소의 스킬 문서를
-            안전하게 구조화할 수 있습니다.
-          </SectionIntro.Description>
-          <SectionIntro.Link href="#">가이드 보러 가기</SectionIntro.Link>
+          <SectionIntro.Description>{t('description')}</SectionIntro.Description>
+          <SectionIntro.Link href="#">{t('link')}</SectionIntro.Link>
         </SectionIntro>
       </Box>
     </Section>
